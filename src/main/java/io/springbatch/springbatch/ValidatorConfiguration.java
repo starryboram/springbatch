308467c204fdaf17ba99546356a1a3b62578163e
starryboram <boramplay@naver.com>
2024-11-15 14:51:20 +0900

    [study] simpleJob - validator

---
+++ src/main/java/io/springbatch/springbatch/ValidatorConfiguration.java
@@ -0,0 +1,75 @@
+package io.springbatch.springbatch;//package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class ValidatorConfiguration{
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+
+    @Bean
+    public Job batchJob() {
+        return new JobBuilder("batchJob", jobRepository)
+                .start(step1())
+                .next(step2())
+                .next(step3())
+    //            .validator(new CustomJobParameterValidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[] {"count"}))
+                .build();
+    }
+
+    @Bean
+    public Step step1(){
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step1 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2(){
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step2 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3(){
+        return new StepBuilder("step3", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step3 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+}
