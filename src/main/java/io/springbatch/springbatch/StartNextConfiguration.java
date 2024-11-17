7f53783fa72cedf3520793fa375fd043d5c3dd4e
starryboram <boramplay@naver.com>
2024-11-17 15:35:34 +0900

    [study] FlowJob - start(), next()

---
+++ src/main/java/io/springbatch/springbatch/StartNextConfiguration.java
@@ -0,0 +1,137 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class StartNextConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+
+    @Bean
+    public Job batchJob() {
+        return new JobBuilder("batchJob", jobRepository)
+                .start(flowA())
+                .next(step3())
+                .next(flowB())
+                .next(step6())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Flow flowA() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
+        flowBuilder.start(step1())
+                .next(step2())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Flow flowB() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
+        flowBuilder.start(step4())
+                .next(step5())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step1 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step2 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step3 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return new StepBuilder("step4", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step4 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return new StepBuilder("step5", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step5 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return new StepBuilder("step6", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" >> step6 was executed!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager)
+                .build();
+    }
+}
+
